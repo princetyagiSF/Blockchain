7b1f334aa677eb9e4697c55156dcee0d21d641b3
Prince Tyagi <42377167+princetyagi258456@users.noreply.github.com>
2022-11-28 11:26:29 +0530

    Create constant.sol

---
+++ Solidity/constant.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+contract constantContract{
+    address public constant ownerAdd = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; //21420  
+    address public ownerAd = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; //23575 
+
+    // With the help of "constant" keyword gas fees take less in compare to the non-constant.
+}
