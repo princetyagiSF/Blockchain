99ed0ddf67ab66d34a7754ad6e6695c7bc29d685
Prince Tyagi <42377167+princetyagi258456@users.noreply.github.com>
2022-11-28 11:22:09 +0530

    Create SolidityTestStateVariable.sol

---
+++ Solidity/SolidityTestStateVariable.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.1;
+contract solidityTestStateVariable{
+    uint storedData; //state variable
+    constructor() public{
+        storedData = 10; //Using State Variable
+    }
+}
