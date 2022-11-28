59640354fee8ef6e664a55fd05725911ab653bee
Prince Tyagi <42377167+princetyagi258456@users.noreply.github.com>
2022-11-28 11:23:31 +0530

    Create SampleDataType.sol

---
+++ Solidity/SampleDataType.sol
@@ -0,0 +1,17 @@
+ // SPDX-License-Identifier: GPL-3.0
+ pragma solidity ^0.8.0;
+
+ // data type  = value & reference
+
+ contract valueType{
+     bool public boolTemp = true;
+
+     uint public temp = 24; // unit used in transfer ehter and gas 
+
+     int public temp1 = 23; // 
+
+     address public addr = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+     bytes32  public b2;
+ }
+
