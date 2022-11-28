5f44f610ad7fbff8932149feaef162860f055af8
Prince Tyagi <42377167+princetyagi258456@users.noreply.github.com>
2022-11-28 11:25:36 +0530

    Create default values.sol

---
+++ Solidity/default values.sol
@@ -0,0 +1,10 @@
+ // SPDX-License-Identifier: GPL-3.0
+ pragma solidity ^0.8.0;
+
+ contract defaultValues{
+     uint public x;
+     bool public y;
+     address public z;
+     bytes32 public a;
+     string public str;
+ }
