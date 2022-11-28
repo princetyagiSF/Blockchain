8f8866ab47d716f15319321048d28acf77ea3d1c
Prince Tyagi <42377167+princetyagi258456@users.noreply.github.com>
2022-11-28 11:10:43 +0530

    Create FirstContract.sol

---
+++ Solidity/FirstContract.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+contract AddTwoNumber
+{
+    int public c ;
+    constructor()
+    {
+        c = 5;
+    }
+    function Add(int a,int b) pure public returns(int)
+    {
+        int sum = a + b;
+        for(int i=0; i<5; i++){
+            
+        }
+        return sum;
+    }
+    
+
+}
